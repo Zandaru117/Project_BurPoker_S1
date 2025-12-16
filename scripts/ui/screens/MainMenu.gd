4aebdd6a75cf94eb70aa66ef6eb1c5376f26f36c
Budazhap <zandakovbuda@gmail.com>
2025-12-16 19:06:45 +0700

    commit 3

--- scripts/ui/screens/MainMenu.gd
+++ scripts/ui/screens/MainMenu.gd
@@ -16,3 +16,6 @@ func _on_play_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Settings.tscn")
